6a5c481505dde0b3b43eb2517650c65b35bd2604
mjseok <mjseok98@gmail.com>
2022-06-01 10:56:13 +0900

    백준 - 단지번호붙이기

--- Python/0524_4.py
+++ Python/0524_4.py
@@ -1,38 +1,38 @@
-#다시 풀어보기
 from collections import deque
-dx=[0,0,1,-1]
-dy=[1,-1,0,0]
+count=[]
 
-def bfs(apartment,a,b):
-    n=len(apartment)
-    queue=deque()
-    queue.append((a,b))
-    apartment[a][b]=0
-    cnt=1
-
-    while queue:
-        x,y=queue.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
+def bfs(appartment,i,j):
+    n=len(appartment)
+    dx=[0,0,1,-1]
+    dy=[1,-1,0,0]
+    visited=deque()
+    visited.append([i,j])
+    appartment[i][j]=0
+    count=1
+    while visited:
+        x,y=visited.popleft()
+        for pos in range(4):
+            nx=dx[pos]+x
+            ny=dy[pos]+y
             if nx<0 or nx>=n or ny<0 or ny>=n:
                 continue
-            if apartment[nx][ny]==1:
-                apartment[nx][ny]=0
-                queue.append((nx,ny))
-                cnt+=1
-    return cnt
+            if appartment[nx][ny]==1:
+                appartment[nx][ny]=0
+                visited.append([nx,ny])
+                count+=1
+    return count
+
 
 N=int(input())
-apartment=[]
+appartment=[]
 for i in range(N):
-   apartment.append(list(map(int,input())))
-count=[]
+    appartment.append(list(map(int,input())))
+
 for i in range(N):
     for j in range(N):
-        if apartment[i][j]==1:
-            count.append(bfs(apartment,i,j))
+        if appartment[i][j]==1:
+            count.append(bfs(appartment,i,j))
 count.sort()
 print(len(count))
-for i in range(len(count)):
-    print(count[i])
+for c in count:
+    print(c)
