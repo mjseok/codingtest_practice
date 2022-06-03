c892ebb316b3d7e99208962894c68333acd48417
mjseok <mjseok98@gmail.com>
2022-06-03 15:24:16 +0900

    백준- 터렛

--- Python/0603_4.py
+++ Python/0603_4.py
@@ -1,61 +1,15 @@
-from collections import deque
-
-R, C = map(int, input().split())
-
-graph = [list(input()) for _ in range(R)]
-# 좌표 위에 뭔가 있으면 True로 방문 처리
-visited = [[False] * C for _ in range(R)]
-
-sonic = deque()  # 고슴도치
-water = deque()  # 물
-count = 0  # 시간(초) 카운트
-
-# 방향 설정
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# 물, 고슴도치 좌표 추가. 방문 True로 설정
-for i in range(R):
-    for j in range(C):
-        if graph[i][j] == '*':
-            water.append((i, j))
-            visited[i][j] = True
-        elif graph[i][j] == 'S':
-            sonic.append((i, j))
-            visited[i][j] = True
-        elif graph[i][j] == 'X':
-            visited[i][j] = True
-
-# 고슴도치 이동가능할때마다 반복문
-while sonic:
-    # 물 현황 구현
-    for i in range(len(water)):
-        w_x, w_y = water.popleft()
-        for j in range(4):
-            nx = w_x + dx[j]
-            ny = w_y + dy[j]
-            if 0 <= nx < R and 0 <= ny < C:
-                if graph[nx][ny] == '.':
-                    water.append((nx, ny))
-                    graph[nx][ny] = '*'
-                    visited[nx][ny] = True
-
-    # 물 확장시 시간 추가. 고슴도치는 물이 찰 예정 칸으로 이동할 수 없기 때문에 고슴도치 구현보다 먼저 카운트
-    count += 1
-
-    # 고슴도치 움직이는 로직 구현
-    for _ in range(len(sonic)):
-        print(graph)
-        s_x, s_y = sonic.popleft()
-        for i in range(4):
-            nx = s_x + dx[i]
-            ny = s_y + dy[i]
-            if 0 <= nx < R and 0 <= ny < C:
-                if graph[nx][ny] =='.' and visited[nx][ny] == False:
-                    sonic.append((nx, ny))
-                    visited[nx][ny] = True
-                elif graph[nx][ny] == 'D':
-                    print(count)
-                    exit()
-
-print('KAKTUS')
+import math
+n=int(input())
+for i in range(n):
+    x1,y1,r1,x2,y2,r2=map(int,input().split())
+    d=math.sqrt((x2-x1)**2+(y2-y1)**2)
+    ## 일치
+    if d==0 and r1==r2:
+        print(-1)
+    # 내접 또는 외접(일치 조건 후에 해야함)
+    elif d==r1+r2 or d==abs(r2-r1):
+        print(1)
+    elif abs(r2-r1)<d<r2+r1:
+        print(2)
+    else:
+        print(0)
