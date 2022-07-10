a3384fd49aed66dce69fd46052c23075e0518473
mjseok <mjseok98@gmail.com>
2022-07-10 23:38:17 +0900

    SWEA - 1974.스도쿠 검증(수정)

--- Python/0710_2.py
+++ Python/0710_2.py
@@ -16,6 +16,16 @@ for tc in range(1,T+1):
         if len(checkR)!=9 or len(checkC)!=9:
             ans=0
             break
+    for i in range(0,9,3):
+        for j in range(0,9,3):
+            check=[]
+            for k in range(3):
+                for l in range(3):
+                    if arr[i+k][j+l] not in check:
+                        check.append(arr[i+k][j+l])
+            if len(check)!=9:
+                ans=0
+                break
     print(f'#{tc}')
     print(ans)
 
