25a6daed808d1bb1ae1e45d3da44ad5c93f7e7dc
mjseok <mjseok98@gmail.com>
2022-09-01 22:36:28 +0900

    다시 풀기

---
+++ Java/Solution_성격유형검사.java
@@ -0,0 +1,11 @@
+
+public class Solution_성격유형검사 {
+	 public String solution(String[] survey, int[] choices) {
+	        String answer = "";
+	        for(String ans : survey) {
+	        	
+	        }
+	       
+	        return answer;
+	    }
+}
